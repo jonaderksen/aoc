bc372d017cbf6597913aeeda1caead966fd3f69b
Jona Derksen <jonaderksen@gmail.com>
2022-12-09 09:07:28 +0100

    Day 9

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 import util.inputAsList
 
+
 fun main(args: Array<String>) {
-    val input = inputAsList(2022, 9, "sample")
+    val input = inputAsList(2022, 10, "sample")
+
 }
+
 
 
--- src/main/kotlin/year2022/day9/Day9.kt
+++ src/main/kotlin/year2022/day9/Day9.kt
@@ -1,4 +1,93 @@
 package year2022.day9
 
+import util.inputAsList
+import util.words
+
 class Day9 {
+
+    fun part1(): Int{
+        return solve( mutableListOf(Pair(0, 0), Pair(0, 0)))
+    }
+
+    fun part2(): Int {
+        return solve(
+            mutableListOf(
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0),
+                Pair(0, 0)
+            )
+        )
+    }
+
+    fun solve(listOfPoints: MutableList<Pair<Int, Int>>): Int {
+        val input = inputAsList(2022, 9, "input")
+        val visited = mutableSetOf<Pair<Int, Int>>()
+
+        input.map { line ->
+            val move = line.words()
+            when (move[0]) {
+                "R" -> {
+                    for (i in 0 until move[1].toInt()) {
+                        listOfPoints[0] = listOfPoints[0].right()
+                        doMove(listOfPoints, visited)
+                    }
+                }
+                "L" -> {
+                    for (i in 0 until move[1].toInt()) {
+                        listOfPoints[0] = listOfPoints[0].left()
+                        doMove(listOfPoints, visited)
+                    }
+                }
+                "D" -> {
+                    for (i in 0 until move[1].toInt()) {
+                        listOfPoints[0] = listOfPoints[0].down()
+                        doMove(listOfPoints, visited)
+                    }
+                }
+                else -> {
+                    for (i in 0 until move[1].toInt()) {
+                        listOfPoints[0] = listOfPoints[0].up()
+                        doMove(listOfPoints, visited)
+                    }
+                }
+            }
+        }
+        return visited.size
+    }
+
+    private fun doMove(listOfPoints: MutableList<Pair<Int, Int>>, visited: MutableSet<Pair<Int, Int>>) {
+        for (index in 1 until listOfPoints.size) {
+            val aroundH = getPossiblePoint(listOfPoints[index - 1])
+            if (!aroundH.contains(listOfPoints[index])) {
+                val aroundT = getPossiblePoint(listOfPoints[index])
+                val curPoint = aroundH.first { aroundT.contains(it) }
+                if (index == listOfPoints.lastIndex) {
+                    visited.add(curPoint)
+                }
+                listOfPoints[index] = curPoint
+            }
+        }
+    }
+
+    private fun getPossiblePoint(point: Pair<Int, Int>): List<Pair<Int, Int>> {
+        return listOf(
+            point, point.right(), point.left(), point.up(), point.down(),
+            point.right().up(), point.left().up(),
+            point.right().down(), point.left().down()
+        )
+    }
+
+    private fun Pair<Int, Int>.left() = this.plus(Pair(-1, 0))
+    private fun Pair<Int, Int>.right() = this.plus(Pair(1, 0))
+    private fun Pair<Int, Int>.up() = this.plus(Pair(0, 1))
+    private fun Pair<Int, Int>.down() = this.plus(Pair(0, -1))
+    operator fun Pair<Int, Int>.plus(p: Pair<Int, Int>): Pair<Int, Int> = Pair(first + p.first, second + p.second)
+
 }
